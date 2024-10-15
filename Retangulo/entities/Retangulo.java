c6d35be83a688e5d6d0d22a6d3f03180a3d786d1
Talala <143460653+talala-dev@users.noreply.github.com>
2024-10-15 17:35:55 -0300

    Update Retangulo.java

--- Retangulo/entities/Retangulo.java
+++ Retangulo/entities/Retangulo.java
@@ -4,15 +4,17 @@ public class Retangulo {
   public double width;
   public double height;
 
-  public double Area(){
+  public double Area() {
     return width * height;
   }
-  public double Perimeter(){
-    double p = 2*(height + width);
+
+  public double Perimeter() {
+    double p = 2 * (height + width);
     return p;
   }
-  public double Diagonal(){
-    double d = Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2) );
+
+  public double Diagonal() {
+    double d = Math.sqrt(Math.pow(height, 2) + Math.pow(width, 2));
     return d;
   }
 }
